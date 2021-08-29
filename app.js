12f476aee78b3507a61b8fcc18228513d7eae583
chaitanya005 <muvvalachaitanya05@gmail.com>
2021-08-29 18:04:33 +0530

    resolving error 23..!

--- app.js
+++ app.js
@@ -11,6 +11,34 @@ app.use(
   )
 );
 
+const indexPath = path.resolve(__dirname, ".", "build", "index.html");
+
+app.get("/returnandrefund", (req, res, next) => {
+  fs.readFile(indexPath, "utf8", (err, htmlData) => {
+    if (err) {
+      console.error("Error during file reading", err);
+      return res.status(404).end();
+    }
+
+    htmlData = htmlData
+      .replace(
+        "<title>Spont Store | Return Refund</title>",
+        `<title>Returnn Refundddd</title>`
+      )
+      .replace("Spont Store", "Returnn Refunddd")
+      .replace(
+        "Online Store for brand new fashion clothing, fresh veggies, groceries, and also order Your favourite food from your favourite restaurant's",
+        "SPont store retun refund __META_OG_DESCRIPTION__"
+      )
+      .replace(
+        "__META_DESCRIPTION__",
+        "SPont store retun refund __META_DESCRIPTION__"
+      );
+
+    return res.send(htmlData);
+  });
+});
+
 app.get("/helo", (req, res) => {
   res.send({ message: "hello world" });
 });
